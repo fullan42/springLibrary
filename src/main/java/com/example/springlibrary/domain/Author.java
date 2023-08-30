e6bbca3cf290d1cdc4736e98ca3e600653da4f41
fullan42 <hasan_-1@outlook.com>
2023-08-30 18:11:10 +0300

    hasan

---
+++ src/main/java/com/example/springlibrary/domain/Author.java
@@ -0,0 +1,26 @@
+package com.example.springlibrary.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String surname;
+
+}

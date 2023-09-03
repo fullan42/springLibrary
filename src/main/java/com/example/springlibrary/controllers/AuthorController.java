e77f6912b219a71b7411c24fee2314adbd4ea65c
fullan42 <hasan_-1@outlook.com>
2023-09-03 18:36:05 +0300

    hasan

--- src/main/java/com/example/springlibrary/controllers/AuthorController.java
+++ src/main/java/com/example/springlibrary/controllers/AuthorController.java
@@ -1,7 +1,31 @@
 package com.example.springlibrary.controllers;
 
+import com.example.springlibrary.requests.CreateAuthorRequest;
+import com.example.springlibrary.requests.DeleteAuthorRequest;
+import com.example.springlibrary.responses.CreateAuthorResponse;
+import com.example.springlibrary.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+    @DeleteMapping("deleteById")
+    public ResponseEntity<Void> deleteById(DeleteAuthorRequest request){
+        authorService.deleteById(request);
+        return new ResponseEntity<>( HttpStatus.OK);
+
+    }
+    @PostMapping("create")
+    public ResponseEntity<CreateAuthorResponse> createAuthorResponse(CreateAuthorRequest request){
+        return new ResponseEntity<>(authorService.createAuthorResponse(request), HttpStatus.CONFLICT);
+    }
 }
--- src/test/java/com/example/springlibrary/SpringLibraryApplicationTests.java
+++ src/test/java/com/example/springlibrary/SpringLibraryApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.springlibrary;
 
+import com.example.springlibrary.requests.CreateAuthorRequest;
+import com.example.springlibrary.services.AuthorService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringLibraryApplicationTests {
+    @Autowired
+    AuthorService authorService;
+
+    public AuthorService getAuthorService() {
+        return authorService;
+    }
 
     @Test
     void contextLoads() {
+        CreateAuthorRequest request = new CreateAuthorRequest();
+        request.setId("hasan");
+        request.setName("hasan");
+        request.setSurname("hasan");
+
+        System.out.println(authorService.createAuthorResponse(request));
     }
 
 }
